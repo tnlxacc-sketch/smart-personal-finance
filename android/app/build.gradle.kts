eaff0f57e9c1e7b7e00daacbcb55639e0c562289
tnlxacc-sketch <tnlx.acc@gmail.com>
2026-09-05 06:26:23 +0700

    Bump Android test version for launcher icon refresh

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.tnlxacc.moji"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1-test-v66"
+        versionCode = 3
+        versionName = "1.0.2-test-iconfix"
     }
 
     buildTypes {
