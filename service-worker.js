59d6a33094f1454d7e5b23aecbb812bebdf9dbe8
tnlxacc-sketch <tnlx.acc@gmail.com>
2026-09-04 16:05:24 +0700

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='spfm-public-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(k=>k.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
