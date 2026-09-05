af6d06839baf75263da294c8a51440697690098b
tnlxacc-sketch <tnlx.acc@gmail.com>
2026-09-05 10:53:54 +0700

    Bump Android test build for native backup save dialog

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.tnlxacc.moji"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3-test-backup-gear"
+        versionCode = 5
+        versionName = "1.0.4-test-native-backup"
     }
 
     buildTypes {
