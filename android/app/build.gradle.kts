49c80a3db0300fd34d5fb80b5a281301c5f350b6
tnlxacc-sketch <tnlx.acc@gmail.com>
2026-09-05 12:15:44 +0700

    Create parallel-install Moji RC1 test package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,11 +5,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.tnlxacc.moji"
+        applicationId = "com.tnlxacc.moji.rc1"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5-commercial-rc1"
+        versionCode = 7
+        versionName = "1.0.5-commercial-rc1-parallel"
     }
 
     buildTypes {
