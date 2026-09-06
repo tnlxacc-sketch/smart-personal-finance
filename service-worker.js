e80b9b7a352aa6b43b3ab10774e00df30ff25001
tnlxacc-sketch <tnlx.acc@gmail.com>
2026-09-07 00:54:41 +0700

    Refresh clean baseline cache after visual parity update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mpf-clean-v2';
+const CACHE='mpf-clean-v3';
 const ASSETS=['./','./index.html','./clean.css','./clean-app.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
